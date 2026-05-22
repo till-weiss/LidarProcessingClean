e0ceb3fcdf58996e257c90af06bb9f59db3b917f
Till Weiss <tiweis001@awi.de>
2026-05-22 16:50:26 +0200

    trialrun

--- trialrun.py
+++ trialrun.py
@@ -19,7 +19,7 @@ def main() -> None:
     config = configuration.Configuration()
 
     # --- EDIT THESE TO YOUR PATHS ---
-    config.run_name = "Inuvik_Airport_2023_rerun"
+    config.run_name = "Tuk_23"
     config.target_area_dir = "/isipd/projects/Response/GIS_RS_projects/Masterarbeit_Till_Weiss/data/area"
     config.las_files_dir = "/isipd/projects/p_planetdw/data/lidar/02_pointclouds/2023"
     config.las_footprints_dir = "/isipd/projects/p_planetdw/data/lidar/03_las_footprints/2023"
@@ -27,7 +27,7 @@ def main() -> None:
     config.results_dir = "/isipd/projects/Response/GIS_RS_projects/Masterarbeit_Till_Weiss/results"
 
     config.resolution = 2 
-    config.overlap = 0.2
+    config.overlap = 0.001 #How much % of a strip is part of the AOI
 
     # --- Preprocessing tuning (aligned with current preprocess implementation) ---
     config.preprocess_chunk_size = 500
